1106c988d5fc6318637e23619f39211d4dd34663
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-06-21 18:48:39 -0300

    Landing-done

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 //import Illustration from '../../assets/illustrationalanding.png';
+import { FaGithub } from 'react-icons/fa';
+import { BiDownload } from 'react-icons/bi';
 import styles from './Landing.module.css';
 
 import ParticlesBackground from '../particlesBackground';
@@ -14,13 +16,22 @@ const Landing: React.FC = () => {
             Hi, I ' m <span> SANTIAGO </span>
           </h1>
           <p>
-            Fullstack Developer A passionate Intermediate Full Stack Developer
-            🚀 having an experience of building responsive Web applications with
-            JavaScript / Reactjs / Nodejs and some other cool libraries and
-            frameworks.
+            A passionate <strong>Full Stack Developer</strong> and{' '}
+            <strong>Audiovisual Designer</strong> having an experience of
+            building responsive Web applications with JavaScript / Reactjs /
+            Nodejs and some other cool libraries and frameworks.
           </p>
-          <button>DOWNLOAD CV</button>
-          <button>GITHUB</button>
+          <div className={styles.btnCotainer}>
+            <button className={styles.btn}>
+              {' '}
+              <BiDownload size={'15px'} />
+              &nbsp; Download<strong> &nbsp; CV</strong>
+            </button>
+            <button className={`${styles.btn} ${styles.btnGithub}`}>
+              <FaGithub size={'15px'} />
+              &nbsp; Github
+            </button>
+          </div>
         </div>
       </div>
     </>
