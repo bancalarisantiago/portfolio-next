9db1537c0d14f222c1a0b44b06042d9f227a4e4a
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-06-24 18:10:05 -0300

    Footer links linked

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,14 +4,27 @@ import styles from './Footer.module.css';
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { HiMail } from 'react-icons/hi';
+import Link from 'next/link';
 const Footer: React.FC = () => {
   return (
     <div>
       <footer className={styles.container}>
         <div className={styles.icons}>
-          <FaGithub className={styles.icon} size={'25px'} />
-          <FaLinkedin className={styles.icon} size={'25px'} />
-          <HiMail className={styles.icon} size={'25px'} />
+          <Link href='https://github.com/bancalarisantiago'>
+            <a target='_black'>
+              <FaGithub className={styles.icon} size={'25px'} />
+            </a>
+          </Link>
+          <Link href='https://www.linkedin.com/in/bancalarisantiago/'>
+            <a target='_black'>
+              <FaLinkedin className={styles.icon} size={'25px'} />
+            </a>
+          </Link>
+          <Link href='/'>
+            <a target='_black'>
+              <HiMail className={styles.icon} size={'25px'} />
+            </a>
+          </Link>
         </div>
       </footer>
     </div>
