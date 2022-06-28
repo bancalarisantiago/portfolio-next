940a67b40985658b8762088c1619aa5f75bd89cb
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-06-28 18:38:39 -0300

    Component about in process

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,15 +1,53 @@
+//Components
+import Image from 'next/image';
 //Styles
 import styles from './About.module.css';
-
+//Images
+import photo from '../../assets/images/fotoPerfilBN.png';
+//Icons
+import { GiTechnoHeart } from 'react-icons/gi';
+import { BsInfoCircle } from 'react-icons/bs';
 const About: React.FC = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.title}>
-          <div className={styles.content}>
-            <h1>
-              <span> SKILLS </span>
-            </h1>
+        <div className={styles.content}>
+          <div className={styles.meContent}>
+            <div className={styles.title}>
+              <div>
+                <h1>ME</h1>
+              </div>
+              <div className={styles.icon}>
+                <BsInfoCircle />
+              </div>
+            </div>
+            <div className={styles.divider}></div>
+            <div className={styles.photo}>
+              <Image
+                src={photo}
+                layout='fixed'
+                width={120}
+                height={120}
+              ></Image>
+            </div>
+            <p>
+              I'm constantly learning and perfecting my skill as a developer
+              every day. I enjoy challenges and goals that force me to increase
+              my knowledge and skills. I like to create things with
+              JavaScript/TypeScript technologies that can help people do their
+              chores easier and faster.
+            </p>
+          </div>
+          <div className={styles.technologiesContent}>
+            <div className={styles.title}>
+              <div>
+                <h1>TECHNOLOGIES</h1>
+              </div>
+              <div className={styles.icon}>
+                <GiTechnoHeart />
+              </div>
+            </div>
+            <div className={styles.divider}></div>
             <p>
               I love working on a variety of technologies including Web
               Development, Mobile App Development and Deep Learning. My goal is
